bec395e5d36c6b98826e3bbf5da76c3c580dc14b
Mitch Robb <olordhux@gmail.com>
2014-03-15 00:19:01 -0700

    up to 5.24

--- Gemfile
+++ Gemfile
@@ -2,10 +2,10 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.3'
-
+gem 'bootstrap-sass'
 
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 4.0.0'
+gem 'sass-rails'
 
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,20 +8,20 @@ describe "StaticPages" do
     it "should have the correct title" do
       StaticPagesController.action_methods.each do |meth|
         #homepage doesn't have a special title
-        path = 'static_pages_'+meth.to_s+'_path'
+        path = meth.to_s+'_path'
         #p "before, page is: #{page.title}"
         visit send(path)
         if meth == "home" #homepage only has the base title
           expect(page).not_to have_title(meth.to_s.capitalize)
         else
-          expect(page).to have_title("#{base_title} | #{meth.to_s.capitalize}")
+          expect(page).to have_title("#{meth.to_s.capitalize} | #{base_title}")
         #p "after visit, page is: #{page}"
         end
       end
     end
     describe "Home page" do
       it "should have the content 'Sample App'" do
-        visit static_pages_home_path
+        visit home_path
         expect(page).to have_content('Sample App')
       end
 =begin
@@ -33,7 +33,7 @@ describe "StaticPages" do
     end
     describe "Help page" do
       it "should have the content 'Help'" do
-        visit static_pages_help_path
+        visit help_path
         expect(page).to have_content('Help')
       end
 =begin
@@ -45,9 +45,10 @@ describe "StaticPages" do
     end
     describe "ABOUT page" do
       it "should have the content 'About Us'" do
-        visit static_pages_about_path
+        visit about_path
+
         expect(page).to have_content('About Us')
-      end
+       end
 =begin
       it "should have the right title" do
         visit static_pages_about_path
@@ -57,7 +58,7 @@ describe "StaticPages" do
     end
     describe "Contact page" do
       it "should have the content 'Contact Us'" do
-        visit static_pages_contact_path
+        visit contact_path
         expect(page).to have_content('Contact Us')
       end
     end
